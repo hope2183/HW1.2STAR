6a1247b88bcf5bbd342a2f5739411ca6529547e9
hope2183@mail.ru <hope2183@mail.ru>
2021-11-20 21:34:00 +1100

    CommitBranch 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -10,18 +10,22 @@ public class Main {
      int c = 44;
      int d = 15;
      int e = 9;
-     int Result = (a * (b + (c - d * e))*(-1));
+     int Result = (a * (b + (c - d * e)))*-1;
      System.out.println(Result);
 
 //TASK7//
 
     int f = 5;
     int g = 7;
-    System.out.println("F = "+g);
-    System.out.println("G = "+f); // с помощью математических действий!!//
+    f = f+g;
+    g = f-g;
+    f=f-g;
+    System.out.println("F = "+f+", G = "+g);
 
 //TASK8//
-
+    int h = 459;
+    int i = (h/10)%10;
+    System.out.println(i);
 
 
 
